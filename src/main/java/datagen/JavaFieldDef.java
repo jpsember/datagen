ad151962403d10b676fa8ecb22c7767e337895f0
Jeff Sember <jpsember@gmail.com>
2022-04-17 16:29:50 -0700

    Issue #14: generating succinct names

--- src/main/java/datagen/JavaFieldDef.java
+++ src/main/java/datagen/JavaFieldDef.java
@@ -4,6 +4,8 @@ import js.data.DataUtil;
 
 public class JavaFieldDef extends FieldDef {
 
+  private static final boolean SUCCINCT_NAMES = true;
+
   @Override
   public String provideSetterName() {
     return DataUtil.lowerFirst(DataUtil.convertUnderscoresToCamelCase(name()));
@@ -16,16 +18,20 @@ public class JavaFieldDef extends FieldDef {
 
   @Override
   public String provideNameStringConstantUnqualified() {
+    if (SUCCINCT_NAMES)
+      return "_" + index();
     return name().toUpperCase();
   }
 
   @Override
   public String provideNameStringConstantQualified() {
-    return name().toUpperCase();
+    return provideNameStringConstantUnqualified();
   }
 
   @Override
   public String provideInstanceName() {
+    if (SUCCINCT_NAMES)
+      return "m" + index();
     return "m" + DataUtil.convertUnderscoresToCamelCase(name());
   }
 
